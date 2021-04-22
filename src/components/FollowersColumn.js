c488f3126f65a5df0d7bac5e3c46ab0295e22d34
root <iarshtejay@gmail.com>
2021-04-22 08:03:41 +0000

    add components

---
+++ src/components/FollowersColumn.js
@@ -0,0 +1,9 @@
+const FollowersColumn = () => {
+    return (
+      <div className="followers-column">
+          <h1>FollowersColumn</h1>
+      </div>
+    );
+  }
+
+export default FollowersColumn
