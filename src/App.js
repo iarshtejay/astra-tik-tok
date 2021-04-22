3bc9eb6553e9985a340e21fb90ebd6829f04e5af
root <iarshtejay@gmail.com>
2021-04-22 07:43:50 +0000

    Step 5 final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import { HashRouter, Route , Switch} from 'react-router-dom'
+import Home from './pages/Home'
+import Upload from './pages/Upload'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+       <Header />
+    <Switch>
+      <Route path= "/upload" component={Upload}/>
+      <Route path= "/" component={Home}/>
+    </Switch>
+  </HashRouter>
   );
 }
 
-export default App;
+export default App
